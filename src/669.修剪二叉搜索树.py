a488a4f831a49d7bb6695f19d6604d87669d98f2
sugarmouse <tanghao19950911@gmail.com>
2023-08-04 19:26:19 +0800

    backup

---
+++ src/669.修剪二叉搜索树.py
@@ -0,0 +1,41 @@
+#
+# @lc app=leetcode.cn id=669 lang=python3
+#
+# [669] 修剪二叉搜索树
+#
+
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def trimBST(self, root: Optional[TreeNode], low: int, high: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        if root.val < low:
+            root = self.trimBST(root.right, low, high)
+        elif root.val > high:
+            root = self.trimBST(root.left, low, high)
+        else:
+            root.left = self.trimBST(root.left, low, high)
+            root.right = self.trimBST(root.right, low, high)
+
+        return root
+
+
+# @lc code=end
