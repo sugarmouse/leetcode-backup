77123aab7c8ea9802a0b0f388989ed3bee338b92
sugarmouse <tanghao19950911@gmail.com>
2023-08-10 04:28:44 +0800

    backup

--- src/206.反转链表.py
+++ src/206.反转链表.py
@@ -12,11 +12,10 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head or not head.next:
+        if head is None or head.next is None:
             return head
         rest = self.reverseList(head.next)
         head.next.next = head
         head.next = None
         return rest
 # @lc code=end
-
--- src/92.反转链表-ii.py
+++ src/92.反转链表-ii.py
@@ -4,19 +4,32 @@
 # [92] 反转链表 II
 #
 
-from typing import Optional
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
 # @lc code=start
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
-# @lc code=end
+        if left == 1:
+            return self.reverseN(head, right)
+        head.next = self.reverseBetween(head.next, left-1, right - 1)
+        return head
 
+    def reverseN(self, head: Optional[ListNode], n: int):
+        """
+        The function `reverseN` recursively reverses the first `n` nodes in a linked list.
+        :return: the new head of the reversed linked list.
+        """
+        if n == 1 or n == 0 or head is None:
+            return head
+        b = self.reverseN(head, n - 1)
+        newHead = head.next
+        head.next = head.next.next
+        newHead.next = b
+        return newHead
+
+# @lc code=end
